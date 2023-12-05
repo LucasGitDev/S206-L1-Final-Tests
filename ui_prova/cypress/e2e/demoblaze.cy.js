85ecb3e3cf3684c214cfc3bb845035409a419911
Lucas <devlucasteles@gmail.com>
2023-12-04 23:38:45 -0300

    feat: :sparkles: adds cypress reports

--- ui_prova/cypress/e2e/demoblaze.cy.js
+++ ui_prova/cypress/e2e/demoblaze.cy.js
@@ -103,7 +103,7 @@ describe("Testando o site https://www.demoblaze.com/index.html", () => {
       "#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary"
     ).click();
 
-    cy.wait(1000);
+    cy.wait(5000);
     cy.get("#nameofuser").should("contain", user);
     cy.get("#logout2").click();
 
