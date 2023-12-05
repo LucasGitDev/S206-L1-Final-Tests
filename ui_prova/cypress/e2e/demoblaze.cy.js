9b874bf07e54752b3f899b4edc1e9e9759aade9c
Samuel Nascimento Filho <samuelonfilho@gmail.com>
2023-12-04 23:30:05 -0300

    FEAT: Add cypress tests

--- ui_prova/cypress/e2e/demoblaze.cy.js
+++ ui_prova/cypress/e2e/demoblaze.cy.js
@@ -1,5 +1,7 @@
 describe("Testando o site https://www.demoblaze.com/index.html", () => {
 
+  const { user, password } = { user: "testeze", password: "123456" };
+
   const randomChoice = () => {
     const random = Math.floor(Math.random() * 7) + 1;
     return random;
@@ -39,7 +41,7 @@ describe("Testando o site https://www.demoblaze.com/index.html", () => {
     );
   });
 
-  it.skip("should remove the first item of list", () => {
+  it("should remove the first item of list", () => {
     cy.openSite();
     cy.get(
       "#tbodyid > :nth-child(1) > .card > .card-block > .card-title > .hrefch"
@@ -52,7 +54,7 @@ describe("Testando o site https://www.demoblaze.com/index.html", () => {
     cy.get("#tbodyid > tr").should("have.length", 0);
   });
 
-  it.skip("should have empty cart", () => {
+  it("should have empty cart", () => {
     cy.openSite();
     cy.get("#cartur").click();
 
@@ -60,5 +62,52 @@ describe("Testando o site https://www.demoblaze.com/index.html", () => {
     cy.get("#tbodyid > tr").should("have.length", 0);
   });
 
+  it("should try to create an account without username", () => {
+    cy.openSite();
+    cy.get("#signin2").click();
+    cy.get("#sign-password").type(password);
+    cy.get(
+      "#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary"
+    ).click();
+
+    // check window alert
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Please fill out Username and Password.");
+    });
+  });
+
+  it("should create an account", () => {
+    cy.openSite();
+    cy.get("#signin2").click();
+    cy.get("#sign-username").type(user);
+    cy.get("#sign-password").type(password);
+    cy.wait(1000);
+    cy.get(
+      "#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary"
+    ).click();
+
+    // check window alert
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Sign up successful.");
+    });
+  });
+
+  it("should login and logout", () => {
+    cy.openSite();
+    cy.wait(2000);
+    cy.get("#login2").click();
+    cy.get("#loginusername").type(user);
+    cy.get("#loginpassword").type(password);
+
+    cy.get(
+      "#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary"
+    ).click();
+
+    cy.wait(1000);
+    cy.get("#nameofuser").should("contain", user);
+    cy.get("#logout2").click();
+
+    cy.get("#login2").should("contain", "Log in");
+  });
   
 });
